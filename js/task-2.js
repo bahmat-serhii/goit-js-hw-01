58a5b58f19318734c8b7a25b43da429bfa8134a7
bahmat <bakhmatov1982@gmail.com>
2025-01-19 18:23:27 +0200

    fix task-2 and task-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
-  const getShippingMessage = `"Shipping to ${country} will cost ${totalPrice} credits"`;
-  return getShippingMessage;
+  const getShippingPrice = `"Shipping to ${country} will cost ${totalPrice} credits"`;
+  return getShippingPrice;
 }
 console.log(getShippingMessage("Australia", 120, 50));
 console.log(getShippingMessage("Germany", 80, 20));
--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,9 @@
 function getElementWidth(content, padding, border) {
-  const getElementWidth =
+  const totalWidth =
     Number.parseFloat(content) +
-    Number.parseFloat(padding) +
-    Number.parseFloat(padding) +
-    Number.parseFloat(border) +
-    Number.parseFloat(border);
-  return getElementWidth;
+    2 * Number.parseFloat(padding) +
+    2 * Number.parseFloat(border);
+  return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
